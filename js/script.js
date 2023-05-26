d5157d6a5111173bd1026949df710951cc2366d6
Gopal Lohar <gopalloharnew@gmail.com>
2023-05-26 09:18:22 +0530

    adding skeletons to page and comments to code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log("Projects - Gopal Lohar");
+
+// define variables and constants
+const repoCardTemplate = document.querySelector(".repo-card-template");
+const repoCardWraper = document.querySelector(".repo-card-wraper");
+const numberOfSkeletons = 4;
+
+// addSkeletons
+function addSkeletons() {
+  repoCardWraper.innerHTML = "";
+  for (let i = 0; i < numberOfSkeletons; i++) {
+    const repoCard = repoCardTemplate.content.cloneNode(true);
+    repoCardWraper.append(repoCard);
+  }
+}
+
+// Code Running Sequence
+addSkeletons();
